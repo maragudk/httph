4ad7e7d67cdbfadd4304486b24267fec08c316d1
Markus Wüstenberg <markus@maragu.dk>
2023-02-22 14:24:41 +0100

    Fix function signature of NoClickjacking middleware

--- httph.go
+++ httph.go
@@ -56,7 +56,7 @@ type Middleware = func(next http.Handler) http.Handler
 // NoClickjacking is Middleware which sets headers to disallow frame embedding and XSS protection for older browsers.
 // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
 // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
-func NoClickjacking(next http.Handler) http.HandlerFunc {
+func NoClickjacking(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-Frame-Options", "deny")
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
