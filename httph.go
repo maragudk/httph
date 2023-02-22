06b955b1633bdfd66fc81047f9b49760c43bced2
Markus Wüstenberg <markus@maragu.dk>
2023-02-22 13:30:02 +0100

    Add NoClickJacking middleware

--- httph.go
+++ httph.go
@@ -47,3 +47,18 @@ func FormHandler[Req any](h func(http.ResponseWriter, *http.Request, Req)) http.
 		h(w, r, req)
 	}
 }
+
+// Middleware is a function that takes an http.Handler and returns an http.Handler.
+// This is a common middleware pattern in net/http.
+type Middleware = func(next http.Handler) http.Handler
+
+// NoClickjacking is Middleware which sets headers to disallow frame embedding and XSS protection for older browsers.
+// See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+// See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-XSS-Protection
+func NoClickjacking(next http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("X-XSS-Protection", "1; mode=block")
+		next.ServeHTTP(w, r)
+	})
+}
